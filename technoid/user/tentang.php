64437c8fd25417db4ccefa3657b5e2a82056a2c1
Intan Sari <intansari070707@gmail.com>
2023-11-04 11:53:28 +0700

    "first commit"

---
+++ technoid/user/tentang.php
@@ -0,0 +1,31 @@
+<?php
+require '../config/config.php';
+
+
+require 'templates/header.php';
+?>
+
+<div class="row bg-white border p-2 mt-5">
+    <div class="col-md ml-3">
+        <h1>Tentang Kami</h1>
+        <p class="lead">Kami adalah apotek yang berkomitmen untuk menyediakan layanan kesehatan yang berkualitas dan menyeluruh 
+            kepada pelanggan kami. Dengan staf yang terlatih dan berpengetahuan, kami siap untuk memberikan informasi dan produk 
+            yang Anda butuhkan untuk menjaga kesehatan Anda. Kami menjadikan keamanan dan kepuasan pelanggan sebagai prioritas utama kami, 
+            dan kami selalu berusaha untuk memberikan pelayanan terbaik dalam dunia farmasi.
+        </p>
+        <hr>
+        <div class="row ml-1">
+            <div class="w-100">
+                <h3><i class="fa fa-map-marker"></i>Alamat</h3>
+                <p>Singosari<br>Malang<br>Jawa Timur<br>Indonesia<br><strong>Intan Sari</strong></p>
+            </div>
+            <!-- /.col-sm-4-->
+            <div class="">
+                <h3><i class="fa fa-phone"></i> Hubungi Kami</h3>
+                <p class="text-muted">Anda dapat menghubungi kami dinomer yang tertera dibawah ini</p>
+                <p><strong>+6289 111 444 222</strong></p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require 'templates/footer.php' ?>
